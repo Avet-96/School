92beb5e6c6cf426ebc68778d6dbfdad7649003f7
Avet-96 <a19962442@mail.ru>
2021-01-31 17:28:47 +0400

    new functions

--- src/components/scool-pages/draw-list/Title.js
+++ src/components/scool-pages/draw-list/Title.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Title = (props) => {
     return (
-        <div><h1>{props.value}</h1></div>
+        <div>
+            <h1>{props.value}</h1>
+        </div>
     )
 }
+
 export default Title
